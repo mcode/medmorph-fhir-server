0fce3830b49ef253edaa73b236943c7be338398b
Benjamin Langley <blangley@mitre.org>
2021-01-22 10:22:53 -0500

    Add response types and scopes

--- src/main/java/ca/uhn/fhir/jpa/starter/wellknown/WellKnownEndpointController.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/wellknown/WellKnownEndpointController.java
@@ -3,6 +3,7 @@ package ca.uhn.fhir.jpa.starter.wellknown;
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,8 @@ public class WellKnownEndpointController {
 
     // Well Known JSON Keys
     private static final String WELL_KNOWN_TOKEN_ENDPOINT_KEY = "token_endpoint";
+    private static final String RESPONSE_TYPES_SUPPORTED_KEY = "response_types_supported";
+    private static final String SCOPES_SUPPORTED_KEY = "scopes_supported";
 
     @PostConstruct
     protected void postConstruct() {
@@ -30,8 +33,17 @@ public class WellKnownEndpointController {
     @GetMapping(path = "/smart-configuration", produces = {"application/json"})
     public String getWellKnownJson(HttpServletRequest theRequest) {
 
+        JSONArray scopesSupported = new JSONArray();
+        scopesSupported.put("system/*.read");
+        scopesSupported.put("offline_access");
+
+        JSONArray responseTypesSupported = new JSONArray();
+        responseTypesSupported.put("token");
+
         JSONObject wellKnownJson = new JSONObject();
         wellKnownJson.put(WELL_KNOWN_TOKEN_ENDPOINT_KEY, HapiProperties.getAuthServerTokenAddress());
+        wellKnownJson.put(RESPONSE_TYPES_SUPPORTED_KEY, responseTypesSupported);
+        wellKnownJson.put(SCOPES_SUPPORTED_KEY, scopesSupported);
 
         return wellKnownJson.toString(2);
     }

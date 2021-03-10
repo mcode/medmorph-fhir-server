5ca47e644040e9989be0cc8a9615caba0e410f93
Dylan <dehall@mitre.org>
2021-03-10 13:47:36 -0500

    some lint

--- wip-libraries/smart-backend-auth/src/main/java/org/mitre/hapifhir/WellknownEndpointHelper.java
+++ wip-libraries/smart-backend-auth/src/main/java/org/mitre/hapifhir/WellknownEndpointHelper.java
@@ -4,15 +4,13 @@ import org.json.JSONObject;
 
 public class WellknownEndpointHelper {
 
-    // Well Known JSON Keys
-    private static final String WELL_KNOWN_TOKEN_ENDPOINT_KEY = "token_endpoint";
-	
-	public static String getWellKnownJson(String tokenEndpointUrl) {
-        JSONObject wellKnownJson = new JSONObject();
+  // Well Known JSON Keys
+  private static final String WELL_KNOWN_TOKEN_ENDPOINT_KEY = "token_endpoint";
 
-        wellKnownJson.put(WELL_KNOWN_TOKEN_ENDPOINT_KEY, tokenEndpointUrl);
+  public static String getWellKnownJson(String tokenEndpointUrl) {
+    JSONObject wellKnownJson = new JSONObject();
+    wellKnownJson.put(WELL_KNOWN_TOKEN_ENDPOINT_KEY, tokenEndpointUrl);
 
-        return wellKnownJson.toString(2);
-	}
-	
+    return wellKnownJson.toString(2);
+  }
 }
